364fe2464d390585c0e2b208a3bea3d68b5862ae
harshita <harshita@progaindia.com>
2022-12-18 13:17:13 +0530

    activet heroku

--- shoppingly/shoppingly/settings.py
+++ shoppingly/shoppingly/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import djnago_heroku
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +127,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-djnago_heroku.settings(locals())
